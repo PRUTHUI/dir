dec9f2a68c8a06826dccf1e4927f3e31b3e7e786
PRUTHUI <60943395+PRUTHUI@users.noreply.github.com>
2020-08-20 23:50:32 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+# This program adds two numbers provided by the user.
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+# Add two numbers.
+sum = num1 + num2
+# Display the sum.
+print(sum)
